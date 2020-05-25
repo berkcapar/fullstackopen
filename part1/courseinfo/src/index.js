4e9b2ab4a06b56da05b28c057e1f463f64bcd634
Berk Çapar <berkcapar@gmail.com>
2020-05-25 14:04:26 +0300

    part 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -34,17 +34,23 @@ const Total = (props) => {
 const App = () => {
   
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  } 
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
     <Header course ={course}/>
-    <Content chapter1 = {part1} exercise1 = {exercises1} chapter2 = {part2} exercise2 = {exercises2} chapter3={part3} exercise3={exercises3} />
-    <Total exercise = {exercises1 + exercises2 + exercises3} />
+    <Content chapter1 = {part1.name} exercise1 = {part1.exercises} chapter2 = {part2.name} exercise2 = {part2.exercises} chapter3={part3.name} exercise3={part3.exercises} />
+    <Total exercise = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
