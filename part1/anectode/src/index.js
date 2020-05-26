ff38f2c46066e3d616539b5db68c227888cf7a7c
Berk Çapar <berkcapar@gmail.com>
2020-05-26 21:36:47 +0300

    1.14 anectodes

--- part1/anectode/src/index.js
+++ part1/anectode/src/index.js
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom'
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
-
+  const [votes, setVotes] = useState([0,0,0,0,0,0])
 
   const handleRandomAnectode = () => {
    setSelected([Math.floor(Math.random()*anecdotes.length)])
-  
   }
+  const handleVoteAnectode = () =>{
 
+    const votesmemory = [...votes]
+    votesmemory[selected]+=1
+    setVotes(votesmemory)
+  }
   return (
     <div>
       <h1>Daily Anectode</h1>
       <p>{anecdotes[selected]}</p>
+      <p>This anectode has {votes[selected]} votes.</p>
       <Button handleClick={handleRandomAnectode} text = "Next Anectode"/>
-
+      <Button handleClick={handleVoteAnectode} text ="Please Vote!" />
+      <h1>Anectode with most votes has {votes[votes.indexOf(Math.max(...votes))]} votes</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]}
     </div>
   )
 }
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
