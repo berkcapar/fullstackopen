2337f83f3ded22fb0e0bd0f11699af20dab37559
Berk Çapar <berkcapar@gmail.com>
2020-07-17 12:55:23 +0300

    part 4.3

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -1,7 +1,14 @@
+
 const dummy = (blogs) => {
     return 1;
   }
+
+
+const totalLikes = (blogs) => blogs.reduce((sum,{likes}) => sum + likes,0)
+
+
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
